e9d8f92d92e52e78017f8e980be7116069ded58a
lzysbl <1369459825@qq.com>
2026-04-10 13:04:47 +0800

    忽略learning目录并取消跟踪其文件

--- learning/redis_test.py
+++
@@ -1,4 +0,0 @@
-import redis
-
-r = redis.Redis(host='127.0.0.1', port=6379, db=0) # db 是redis的分区，相当于mysql的use xx
-print(r.ping())  # 应返回 True
--- learning/字符串.py
+++
@@ -1,7 +0,0 @@
-a = input()
-split_a = a.split()
-print(split_a[0])
-print(split_a[1])
-
-a+= '1'
-print(a)
